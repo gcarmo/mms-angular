6673c1ad8ca5a3432e2e2fa86b0bd3037cd797a7
Guilherme Luiz do Carmo Reis <gcarmo@cit>
2019-11-05 13:02:07 -0300

    Initial project commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'myapp';
+}
+
+
